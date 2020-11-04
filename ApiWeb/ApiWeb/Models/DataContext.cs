be2aa9b02cb49afcec7a18a9c84446172f3a49d4
Anahi Samantha Agreda Cruz <a2019118777@estudiantes.upsa.edu.bo>
2020-11-03 20:48:40 -0400

    Se creo la classe datacontext y el controlador students

---
+++ ApiWeb/ApiWeb/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿
+namespace ApiWeb.Models
+{
+    using System.Data.Entity;
+    public class DataContext : DbContext
+    {//ctor tab tab
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<ApiWeb.Models.Student> Students { get; set; }
+    }
+}
